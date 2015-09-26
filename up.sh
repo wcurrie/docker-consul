ec1de3404f85c8ae36c1bb434362b9043d9d3ccd
Will Currie <will@currie.id.au>
2015-09-26 19:28:02 +1000

    Add names

--- up.sh
+++ up.sh
@@ -4,17 +4,18 @@
 # start 3 servers
 for i in {1..3}
 do 
-    docker run -d --name srv-$i gliderlabs/consul-server -bootstrap-expect 3 
-    export srv$i=$(dip srv-1)
+    docker run --detach --publish-all --name srv-$i --hostname srv-$i gliderlabs/consul-server -bootstrap-expect 3 
+    ip=$(dip srv-$i)
+    export srv$i=$ip
+    echo srv-$i $ip
 done
 
 docker exec srv-2 consul join $srv1
 docker exec srv-3 consul join $srv1
 
-echo srv-{1..3} $srv{1..3}
 
 # start 3 clients
 for i in {1..3}
 do 
-    docker run -d --name agent-$i gliderlabs/consul-agent -retry-join $srv1
+    docker run --detach --name agent-$i --hostname agent-$i gliderlabs/consul-agent -retry-join $srv1
 done
