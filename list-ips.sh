51ce40a22f20bf636d7efe484038a07977851637
Will Currie <will@currie.id.au>
2015-09-26 19:52:30 +1000

    List all agents IPs

---
+++ list-ips.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+. funcs
+
+for who in {srv,agent}
+do
+    for i in {1..3}
+    do
+        container=$who-$i
+        ip=$(dip $container)
+        echo $container $ip
+        export $who$i=$ip
+    done
+done
